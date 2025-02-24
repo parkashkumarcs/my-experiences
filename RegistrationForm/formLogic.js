8279c60a4adbcbe7b68517a5ea70ba5b449dde01
Parkash Kumar <parkashkumar.bscs.official@gmail.com>
2025-02-24 21:51:06 +0500

    update3 Registration_Page 02/24/2025

--- RegistrationForm/formLogic.js
+++ RegistrationForm/formLogic.js
@@ -1,10 +1,20 @@
 
 var getPassword1 = document.getElementById('password1');
 var getPassword2 = document.getElementById('password2');
+var fullName = '';
+var userName = '';
+var length3 = '';
+var length4 = '';
+var passData = getPassword1.value;
+
+// alert(length3)
+// alert(length3.length)
 function togglePassowd(){
     // alert('Working')
-   let extractPaaword1 = getPassword1.getAttribute('type');
-   let extractPaaword2 = getPassword2.getAttribute('type');
+    let extractPaaword1 = getPassword1.getAttribute('type');
+    let extractPaaword2 = getPassword2.getAttribute('type');
+    passData = getPassword1.value;
+    length3 = passData.length;
     // alert(extractPaaword1)
     // alert(extractPaaword2)
     // alert(getPassword1.value)
@@ -27,11 +37,15 @@ function togglePassowd(){
     }
 
 }
-var flag;
+var flag =0;
+let pass2Data = getPassword2.value;
+length3 = passData.length;
+length4 = pass2Data.length;
+alert(length4)
 function matchPassword(currentValue){
-    
     let string = currentValue.value;
     let string2 = getPassword1.value;
+    length4 = string.length;
     if(string.length > 0 && string2.length > 0){
         if(getPassword1.value === string){
             document.getElementById('error').innerHTML = 'passowrd matched';
@@ -50,13 +64,67 @@ function matchPassword(currentValue){
     }
 }
 function register(){
-    if(flag == 1){
-        // alert(flag)
-        alert("Successfully Submited!")
-        return true;
+    let length1 = fullName.length;
+    let length2 = userName.length;
+    alert(length3)
+    alert(length4)
+    if(length1 > 0 && length2 > 0 && length3 > 0 && length4 > 0){
+        if(flag == 1){
+            // alert(flag)
+            alert("Successfully Submited!")
+            return true;
+        }
+    }else{
+        if(length1 == 0){
+            let changeColor = document.getElementById('field1');
+            // alert('color')
+            let ca = changeColor.getAttribute('class');
+            // alert(ca)
+            if(ca === 'input'){
+                changeColor.setAttribute('class', 'errorField');
+                // alert(changeColor.getAttribute('class'))
+            }
+        }
+        if(length2 == 0){
+            let changeColor = document.getElementById('field2');
+            // alert('color')
+            let ca = changeColor.getAttribute('class');
+            // alert(ca)
+            if(ca === 'input'){
+                changeColor.setAttribute('class', 'errorField');
+                // alert(changeColor.getAttribute('class'))
+            }
+        }
+        if(length3 == 0){
+            // alert('color')
+            let ca = getPassword1.getAttribute('class');
+            // alert(ca)
+            if(ca === 'input'){
+                getPassword1.setAttribute('class', 'errorField');
+                // alert(getPassword1.getAttribute('class'))
+            }
+        }
+        if(length4 == 0){
+            // alert('color')
+            let ca = getPassword2.getAttribute('class');
+            // alert(ca)
+            if(ca === 'input'){
+                getPassword2.setAttribute('class', 'errorField');
+                // alert(getPassword2.getAttribute('class'))
+            }
+        }
+        
     }
+    
     if(flag == 0){
         // alert(flag)
         return false;
     }
+}
+
+function required1(vl){
+    fullName = vl.value;
+}
+function required2(vl){
+    userName = vl.value;
 }
